e13cd89b763ecb2dd9f0ce28f3c06efba6c4635d
Jonathan Binns <jonny.binns1@gmail.com>
2019-10-19 14:23:24 +0100

    fully set up, hopefully

--- RouteFinder.java
+++ RouteFinder.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class RouteFinder {
 
 	public static void main(String[] args) {
+		/** for running in cmd
 		try{
 	 			String cavesFile = args[0];
-				//System.out.println(file);
 				File caves = new File(cavesFile);
 				String cavesStr = "";
 				Scanner myReader = new Scanner(caves);
@@ -23,7 +23,28 @@ public class RouteFinder {
 		{
 				System.out.println("Java did an o o p s i e.");
 				e.printStackTrace();
+		}
+		 **/
+		
+		
+		try{
+			File caves = new File("input1.cav");
+			String cavesStr = "";
+			Scanner myReader = new Scanner(caves);
+			while(myReader.hasNextLine())
+			{
+				cavesStr = myReader.nextLine();
 			}
+			myReader.close();
+
+			System.out.println(cavesStr);
+	}
+	catch (FileNotFoundException e)
+	{
+			System.out.println("Java did an o o p s i e.");
+			e.printStackTrace();
+	}
+		
 
 	}
 
